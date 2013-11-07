297b0599ed0f75a0ddf3c4fcc9f5555f4f3f3b59
Ben Brandt <benbrandt@gmail.com>
2013-11-06 20:58:17 -0600

    Created with Android Studio with master/detail layout Activity

---
+++ V1TaskList/src/main/java/com/lizzben/v1tasklist/model/Task.java
@@ -0,0 +1,19 @@
+package com.lizzben.v1tasklist.model;
+
+/**
+ * V1 Task
+ */
+public class Task {
+    public String id;
+    public String content;
+
+    public Task(String id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return content;
+    }
+}
